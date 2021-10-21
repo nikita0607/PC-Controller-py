4ff71cc33390a180f443c4ace8bacd392a546101
nikita0607 <ecfed205@gmail.com>
2021-10-22 01:25:04 +0800

    Create API class Create Methods class

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 
@@ -7,34 +9,51 @@ class API:
 
         self.button = ButtonMethods(self)
         self.adr = None
+
+    def jsonify(self, data) -> str:
+        return json.dumps(data)
     
     def connect(self, user_name: str, computer_name: str, ip: str):
         data = {"user_name": user_name, "name": computer_name}
         self.adr = f"{ip}:5000/a"
 
-        actions = self.session.post(f"{ip}:5000/a", data=data).json()
+        return self.response(data)
+
+    def response(self, data: dict, get_actions: bool = False) -> dict:
+        data["get_actions"] = get_actions
+
+        actions = self.session.post(self.adr, json=self.jsonify(data)).json()
         return actions
 
     def call_method(self, method: str, get_actions: bool = False, **kwargs):
-        data = {"method": method, "get_actions": get_actions}
+        data = {"action": "method", "type": method, "get_actions": get_actions}
         data.update(kwargs)
 
-        actions = self.session.post(self.adr, data=data).json()
-        return actions
+        return self.response(data, get_actions)
 
     def get_actions(self):
-        actions = self.session.post(self.adr).json()
+        actions = self.response({}, True)
         return actions
 
 
-class ButtonMethods:
+class Methods:
     def __init__(self, parrent_api: API):
         self.parrent = parrent_api
 
+
+class ButtonMethods(Methods):
     def add(self, button_name: str, button_text: str):
         data = {"name": button_name, "text": button_text}
 
-        self.parrent.call_method("button.add", **data)
+        return self.parrent.call_method("button.add", **data)
+
+    def delete_all(self):
+        return self.parrent.call_method("button.delete_all")
+
+
+class ComputerMethods(Methods):
+    def disconnect(self):
+        return self.parrent.call_method("computer.disconnect")
 
 
 if __name__ == "__main__":
