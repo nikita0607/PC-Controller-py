8a340792130d71178ce3d58f1a5e823b63faf3a3
nikita0607 <ecfed205@gmail.com>
2021-10-22 01:26:49 +0800

    Create test file main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import lib
 
+from time import sleep
+
 
 api = lib.API()
-api.connect("nikita0607", "Test")
+api.connect("nikita0607", "Test", "http://192.168.0.102")
 
-api.button.add("Test", "Test")
+api.button.delete_all()
+ans = api.call_method("button.add")
+print(ans)
+
+while True:
+    sleep(2)
+
+    print(api.get_actions())
